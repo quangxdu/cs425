ef72e70f4b41675f7b2949a043ffcace386ff51a
Kevin Hou <hou.kevin@hotmail.com>
2015-03-13 12:14:01 -0500

    Added more client functions

--- client.py
+++ client.py
@@ -6,7 +6,11 @@ Indentations are 4 spaces wide
 '''
 #!/usr/bin/env python
 
-import sys, socket
+import sys, socket, queue
+
+#Global variables
+packetQueue = queue()
+UDP_PORT = 31337
 
 class PackStruct:
     pass
@@ -15,13 +19,17 @@ class Client:
     def __init__(self):
         pass
     
+    def listenToTerminal(self):
+        #Check top value of queue for matching time
+        pass
+    
     def addPacket(self, msg, dest):
         packet = PackStruct()
         packet.message = msg
         packet.dest = dest
+        packetQueue.put(packet)
 
-        UDP_PORT = 31337
-
+    def sendPacket(self, packet):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect((packet.dest, UDP_PORT))
         s.send(packet.message)
