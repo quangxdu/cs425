c024fc163c4a589deeaa4adfa2e1c68e1b3ea602
Kevin Hou <hou.kevin@hotmail.com>
2015-03-16 15:31:00 -0500

    Added Server functionality

--- server.py
+++ server.py
@@ -7,20 +7,29 @@ client multithread- take input and create packets to send to all 3 others
 delay multithread- take created packets and send at correct time
 '''
 
-import datetime, random, socket
+import datetime, random, socket, client
 
 maxdelay = 3
 
-def update(self):
-	while True:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		data = s.recv(1024)
-		s.close()
-		print "Received data: ", data, " System time is: ", returnTime();
-
-
-def returnTime():
-	return datetime.datetime.now();
-def delayTime():
-	return datetime.datetime.now() + datetime.timedelta(seconds = random.uniform(0,maxdelay));
+class Server:
+	database = {}
+	def update(self):
+		while True:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			data = s.recv(1024)
+			s.close()
+			print "Received data: ", data, " System time is: ", Server.returnTime()
+			message = data.split()
+			if(message[0] == "delete"):
+				del self.database[message[1]]
+			elif(message[0] == "get"):
+				value = self.database[message[1]]
+				#Some method to send out return message
+			elif(message[0] == "insert" or message[0] == "update"):
+				self.database[message[1]] = message[2]
+				#Some method to update other servers
+	def returnTime(self):
+		return datetime.datetime.now();
+	def delayTime(self):
+		return datetime.datetime.now() + datetime.timedelta(seconds = random.uniform(0,maxdelay));
 
