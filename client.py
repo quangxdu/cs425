161a8176e21b83de44dc125268807a8c2ad899f2
Kevin Hou <hou.kevin@hotmail.com>
2015-03-16 19:07:37 -0500

    D

--- client.py
+++ client.py
@@ -37,7 +37,8 @@ class Client:
         packet.dest = dest
         currTime = datetime.datetime.now()
         packet.time = currTime
-        randtemp = random.randint(0,3)
+        randtemp = random.uniform(0,3)
+        delay = datetime.timedelta(seconds = randtemp)
         packet.sendTime = currTime + randtemp
         packet.port = dest
         sys.stdout.write("Sent: "+msg+" to "+str(dest)+", System time is +" +currTime.ctime()+"\n")
