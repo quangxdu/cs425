24e5a0674fe1f856f6288dc7a1c92e0d5916e290
quangxdu <quangxdu@gmail.com>
2015-04-11 18:27:17 -0500

    added pseudocode

--- MP2/coordinator.py
+++ MP2/coordinator.py
@@ -1,7 +1,36 @@
-
 '''
-@author Sam
-
+@author Sam Du
 '''
+import sys, math
+
 class coordinator:
+	nodeList = {}
 	
+	def removeNode(num):
+		#complicated shit
+	def addNode(num):
+		#complicated shit
+	def findKey(num,key):
+		#complicated-ish shit
+	def show(num):
+	def showAll:
+		#print entire dictionary
+	
+	'''
+	main thread is coordinator, which reads from terminal for commands.
+	each node is contained in the nodeList, which runs as a separate thread.
+	node will contain keys.
+	Initially we have node 0 which contains 0-255 keys 
+	'''
+	
+	'''
+	removeNode will contain functions
+	moveKeysRM()
+	deleteNode()
+	
+	addNode will contain functions
+	createNode()
+	moveKeysADD()
+	
+	findKey will first go to node num, then call function lookup, which will call lookup(nkey) on new node.
+	'''
