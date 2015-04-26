c7696f5cfdd5f1adc33b195098af6a360303a354
Kevin Hou <hou.kevin@hotmail.com>
2015-04-25 20:38:36 -0500

    Node

---
+++ mp3/node.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 25, 2015
+
+@author: Kevin
+'''
+
+import  Queue
+
+class node:
+    neighbors = []
+    
+    def __init__(self, node_num, neighbor1, neighbor2, neighbor3, neighbor4):
+        self.neighbors.append(node_num)
+        self.neighbors.append(neighbor1)
+        self.neighbors.append(neighbor2)
+        self.neighbors.append(neighbor3)
+        self.neighbors.append(neighbor4)
