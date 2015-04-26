08e3e1a376f126cea7f13282ac7452b5b2b5b2f9
Kevin Hou <hou.kevin@hotmail.com>
2015-04-25 21:16:01 -0500

    Square brackets

--- mp3/mutex.py
+++ mp3/mutex.py
@@ -20,15 +20,15 @@ q7 = Queue.Queue()
 q8 = Queue.Queue()
 q9 = Queue.Queue()
 
-nodelist1= {q1,q2,q3,q4,q7}
-nodelist2= {q2,q1,q3,q5,q8}
-nodelist3= {q3,q1,q2,q6,q9}
-nodelist4= {q4,q1,q7,q5,q6}
-nodelist5= {q5,q4,q6,q2,q8}
-nodelist6= {q6,q4,q5,q3,q9}
-nodelist7= {q7,q8,q9,q1,q4}
-nodelist8= {q8,q7,q9,q2,q5}
-nodelist9= {q9,q7,q8,q3,q6}
+nodelist1= [q1,q2,q3,q4,q7]
+nodelist2= [q2,q1,q3,q5,q8]
+nodelist3= [q3,q1,q2,q6,q9]
+nodelist4= [q4,q1,q7,q5,q6]
+nodelist5= [q5,q4,q6,q2,q8]
+nodelist6= [q6,q4,q5,q3,q9]
+nodelist7= [q7,q8,q9,q1,q4]
+nodelist8= [q8,q7,q9,q2,q5]
+nodelist9= [q9,q7,q8,q3,q6]
 
 n1 = node.node(nodelist1,cs_int,next_req)
 n2 = node.node(nodelist2,cs_int,next_req)
--- mp3/node.py
+++ mp3/node.py
@@ -8,15 +8,18 @@ import  Queue
 from cs425.mp3.mutex import cs_int
 
 class node:
-    neighbors = []
+    queues = []
     cs_int = 0
     next_req = 0
-    
+    value = 0
+    option = 0
 
     
-    def __init__(self, node_num, cs_int, next_req):
+    def __init__(self, node_num, list, cs_int, next_req, option):
+        self.option = option
+        self.value = node_num
         for i in range(0, 5):
-            self.neighbors.append(node_num[i])
+            self.neighbors.append(list[i])
         self.cs_int = cs_int
         self.next_req = next_req
         
