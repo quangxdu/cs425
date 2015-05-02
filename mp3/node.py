8a7793dfa7f8d28767c28f2ce390c7e26c42ab84
Kevin Hou <hou.kevin@hotmail.com>
2015-05-02 12:06:46 -0500

    Changes

--- mp3/node.py
+++ mp3/node.py
@@ -4,16 +4,18 @@ Created on Apr 25, 2015
 @author: Kevin
 '''
 
-import  Queue,Thread
+import  Queue, Thread, datetime
 from cs425.mp3.mutex import cs_int
 
 
 class node(Thread):
+    busy_flag = 0
     neighbors = []
     cs_int = 0
     next_req = 0
     value = 0
     option = 0
+    nextAccessTime = 0
 
     
     def __init__(self, node_num, list, cs_int, next_req, option):
@@ -23,6 +25,18 @@ class node(Thread):
             self.neighbors.append(list[i])
         self.cs_int = cs_int
         self.next_req = next_req
+    
+    def getFlag(self):
+        return self.busy_flag
+    
+    def requestCritical(self):
+        return
 
     def run(self):
         while(True):
+            currTime = datetime.datetime.now()
+            self.nextAccessTime = currTime + datetime.timedelta(seconds=self.next_req)
+            while(True):
+                if(datetime.datetime.now() >= self.nextAccessTime):
+                    self.requestCritical()
+                    break
